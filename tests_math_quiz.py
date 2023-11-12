bc221a5668e5f540b8a94771d9143ba9c94b1efe
vaibhav17ry <150603443+vaibhav17ry@users.noreply.github.com>
2023-11-12 21:47:40 +0100

    Update tests_math_quiz.py

--- tests_math_quiz.py
+++ tests_math_quiz.py
@@ -16,23 +16,24 @@ class TestMathGame(unittest.TestCase):
         # TODO
         listOps=['+' , '-' , '*' , '/' , '%']
         for i in listOps:
-            available_ops = function_ops(random.choice(['+', '-', '*']))
-            self.assertTrue(i==available_ops)
+            available_operation = function_operation(random.choice(['+', '-', '*']))
+            self.assertTrue(i==available_operation)
         pass
 
     def test_function_Calculation(self):
-            test_cases = [
-                (3, 2, '+', '3 + 2', 5),
-                (5, 5, '-', '5 - 5', 0),
-                (8, 5, '*', '8 * 5', 40),
-                (6, 6, '+', '6 + 6', 12)
-            ]
+        test_cases = [
+    (3, 2, '+', '3 + 2', 5),
+    (5, 5, '-', '5 - 5', 0),
+    (8, 5, '*', '8 * 5', 40),
+    (6, 6, '+', '6 + 6', 12)
+                     ]
 
-            for num_1, num_2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                check_vals = function_Calculation(num_1, num_2, operator)
-                self.assertTrue(expected_answer == ANSWER )
-                pass
+       for num_1, num_2, operator, expected_problem, expected_answer in test_cases:
+           calculated_problem, calculated_answer = function_Calculation(num_1, num_2, operator)
+           self.assertTrue (calculated_problem == expected_problem)
+           self.assertTrue (calculated_answer == expected_answer)
+           print(f"Test Passed: {calculated_problem} = {calculated_answer}")
+
 
 if name == "main":
     unittest.main()
