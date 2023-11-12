764309fa604b8908efc57c9d0fb09169b6a46627
vaibhav17ry <150603443+vaibhav17ry@users.noreply.github.com>
2023-11-12 21:12:26 +0100

    Update tests_math_quiz.py

--- tests_math_quiz.py
+++ tests_math_quiz.py
@@ -1,30 +1,38 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import function_Random, function_operation, function_Calculation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_function_random(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+        for i in range(1000):  # Test a large number of random values
+            rand_num = function_ops(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def test_function_operation(self):
         # TODO
+        listOps=['+' , '-' , '*' , '/' , '%']
+        for i in listOps:
+            available_ops = function_ops(random.choice(['+', '-', '*']))
+            self.assertTrue(i==available_ops)
         pass
 
-    def test_function_C(self):
+    def test_function_Calculation(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (3, 2, '+', '3 + 2', 5),
+                (5, 5, '-', '5 - 5', 0),
+                (8, 5, '*', '8 * 5', 40),
+                (6, 6, '+', '6 + 6', 12)
             ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            for num_1, num_2, operator, expected_problem, expected_answer in test_cases:
                 # TODO
+                check_vals = function_Calculation(num_1, num_2, operator)
+                self.assertTrue(expected_answer == ANSWER )
                 pass
 
-if __name__ == "__main__":
+if name == "main":
     unittest.main()
